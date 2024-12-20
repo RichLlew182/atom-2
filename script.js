366c541ae141f739246e53b38c1c5b5e55e1cba0
Richard Llewellyn <richard.llewellyn@webdadi.com>
2024-12-20 10:38:43 +0000

    changed click event to for each as clicking inbetween buttons was causing styles to be removed due to event propagation

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const buttons = document.querySelector('#theme-picker');
+const buttons = document.querySelectorAll('#theme-picker button');
 
-buttons.addEventListener('click', (e) => {
+buttons.forEach((button) => button.addEventListener('click', function () {
 
-    console.log(e.target.id)
-
-    const id = e.target.id;
+    const id = button.id
 
     document.body.className = document.body.className.replace(/\btheme-\S+\b/g, '').trim();
 
-    // Add the ID of the clicked button as a class
     if (id) {
         document.body.classList.add(id);
     }
 
-})
+}))
+
+// buttons.addEventListener('click', (e) => {
+
+// console.log(e.target.id)
+
+// const id = e.target.id;
+
+// document.body.className = document.body.className.replace(/\btheme-\S+\b/g, '').trim();
+
+// // Add the ID of the clicked button as a class
+// if (id) {
+//     document.body.classList.add(id);
+// }
+
+// })
